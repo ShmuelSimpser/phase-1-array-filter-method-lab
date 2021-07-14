fa76222793cbc0d59e700fc055437248f804b1fb
shmuelsimpser <ssimpser@gmail.com>
2021-07-13 21:00:50 -0400

    commit tree 88c4c9cbe3cdf094bb4c5004cb8e0a56c41a4ba9

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+
+function findMatching(bobby, jake) {
+    return bobby.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === jake.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(john, words) {
+    return john.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(words.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(jerry, larry) {
+    return jerry.filter( record => record.name === larry)
+  }
